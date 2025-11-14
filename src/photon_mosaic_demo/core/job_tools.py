ac20171c3123af9c20edf67c441884770dad3a49
Arielle Leon <ariellel@alleninstitute.org>
2025-11-14 15:06:12 -0800

    fix: import the correct object

--- src/photon_mosaic_demo/core/job_tools.py
+++ src/photon_mosaic_demo/core/job_tools.py
@@ -3,6 +3,6 @@ import numpy as np
 
 
 from spikeinterface.core.job_tools import (
-    ensure_n_jobs, ChunkExecutor, chunk_duration_to_chunk_size
+    ensure_n_jobs, ChunkRecordingExecutor, chunk_duration_to_chunk_size
 )
 from spikeinterface.core.core_tools import convert_string_to_bytes, convert_bytes_to_str, convert_seconds_to_str
